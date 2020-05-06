99a353df7f19c42f590bcc5f63ac64af8cd5033c
Adam <TehTarget@hotmail.com>
2020-05-06 01:06:44 -0500

    Add autosave framework for Timesheet

--- src/Atom.TimeTracker/Atom.TimeTracker/ClientApp/src/components/TimeSheets/TimeSheetDetail.js
+++ src/Atom.TimeTracker/Atom.TimeTracker/ClientApp/src/components/TimeSheets/TimeSheetDetail.js
@@ -4,7 +4,16 @@ import axios from 'axios';
 import { DebounceInput } from 'react-debounce-input';
 
 export class TimeSheetDetail extends Component {
-    state = { loading: true, timeSheetId: null, SubmittedDateTime: null, timePeriod: null, entries: null, errorMsg: null };
+    state = {
+        loading: true,
+        timeSheetId: undefined,
+        SubmittedDateTime: undefined,
+        timePeriod: undefined,
+        entries: undefined,
+        errorMsg: undefined,
+        saving: false,
+        hasChanged: false,
+    };
 
     schema = {
         timeSheetId: Joi.number().integer().min(1),
@@ -36,8 +45,18 @@ export class TimeSheetDetail extends Component {
                 const timeSheet = r.data;
 
                 // Add a key field for internal tracking, as when we add new entried during editing, we will not have the Id to use as a key.
-                timeSheet.entries.forEach((e) => (e.key = TimeSheetDetail.uuidv4()));
-                this.setState({ submittedDateTime: timeSheet.submittedDateTime, entries: timeSheet.entries, timePeriod: timeSheet.timePeriod, loading: false });
+                const entries = timeSheet.entries.map((e) => {
+                    return {
+                        id: e.id,
+                        key: TimeSheetDetail.uuidv4(),
+                        note: e.note,
+                        pop: e.percentOfPeriod,
+                        value: e.value,
+                        projectId: e.project.id,
+                        projectName: e.project.name,
+                    };
+                });
+                this.setState({ submittedDateTime: timeSheet.submittedDateTime, entries, timePeriod: timeSheet.timePeriod, loading: false });
             })
             .catch((error) => {
                 if (error.response) {
@@ -53,18 +72,46 @@ export class TimeSheetDetail extends Component {
             });
     }
 
+    componentDidUpdate(nextProps) {
+        const { hasChanged } = this.state;
+        if (hasChanged) {
+            if (this.timeout) {
+                clearTimeout(this.timeout);
+            }
+            this.timeout = setTimeout(this.save, 5000);
+        }
+    }
+
+    sleep = (milliseconds) => {
+        return new Promise((resolve) => setTimeout(resolve, milliseconds));
+    };
+
+    save = async () => {
+        const values = this.state.entries;
+        console.log('Saving', values);
+        clearTimeout(this.timeout);
+        this.setState({ saving: true, hasChanged: false });
+
+        await this.sleep(1000);
+        this.setState({ saving: false });
+    };
+
     handleEntryChange = (e, entry) => {
-        console.log(e.type);
-        console.log(e.name);
-        console.log(e.value);
-        console.log(entry);
+        let entries = [...this.state.entries];
+        const index = entries.indexOf(entry);
+        let newValue = { ...entry };
+
+        newValue[e.name] = e.value;
+
+        entries[index] = newValue;
+        this.setState({ entries, hasChanged: true });
     };
 
     handleEntryCreate = () => {
         let entries = [...this.state.entries];
 
-        entries.push({ key: TimeSheetDetail.uuidv4(), id: null, note: null, value: 0, percentOfPeriod: 0, project: { id: null, name: null } });
-        this.setState({ entries });
+        entries.push({ key: TimeSheetDetail.uuidv4(), id: null, note: undefined, value: 0, percentOfPeriod: 0, projectId: undefined, projectName: undefined });
+        this.setState({ entries, hasChanged: true });
     };
 
     handleEntryDelete = (e) => {
@@ -72,7 +119,7 @@ export class TimeSheetDetail extends Component {
         const index = entries.indexOf(e);
         if (index > -1) {
             entries.splice(index, 1);
-            this.setState({ entries });
+            this.setState({ entries, hasChanged: true });
         }
     };
 
@@ -102,7 +149,7 @@ export class TimeSheetDetail extends Component {
     renderEntityRow = (entry) => {
         return (
             <tr key={entry.id}>
-                <td>{entry.project.name}</td>
+                <td>{entry.projectName}</td>
                 <td>{entry.note}</td>
                 <td>{entry.value}</td>
                 <td>{entry.percentOfPeriod}</td>
@@ -142,16 +189,27 @@ export class TimeSheetDetail extends Component {
             <div>
                 {TimeSheetDetail.renderHeading(this.state)}
                 <hr />
-                <h3>
-                    Entries
-                    <span className="float-right">
-                        {!this.state.submittedDateTime && (
-                            <button className="btn btn-sm btn-success" onClick={this.handleEntryCreate}>
-                                Submit TImeSheet
-                            </button>
+                <div className="row">
+                    <div className="col-2">
+                        <h3>Entries</h3>
+                    </div>
+                    <div className="col ">
+                        {this.state.saving ? (
+                            <span className="badge badge-warning align-middle">
+                                <span className="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span> Saving
+                            </span>
+                        ) : (
+                            !this.state.hasChanged && <span className="badge badge-light align-middle">Saved</span>
                         )}
-                    </span>
-                </h3>
+                        <span className="float-right">
+                            {!this.state.submittedDateTime && (
+                                <button className="btn btn-sm btn-success" onClick={this.handleEntryCreate}>
+                                    Submit TImeSheet
+                                </button>
+                            )}
+                        </span>
+                    </div>
+                </div>
                 <div>{this.renderEntityTable()}</div>
                 <span>
                     {!this.state.submittedDateTime && (
@@ -185,21 +243,20 @@ export class TimeSheetDetailNewEntry extends Component {
             <tr>
                 <td>
                     <DebounceInput
-                        name="productName"
+                        name="projectName"
                         className="form-control form-control-sm"
                         minLength={2}
                         debounceTimeout={500}
-                        project={this.props.entry.project}
-                        value={this.props.entry.project.name}
+                        value={this.props.entry.projectName}
                         onChange={(e) => this.props.onChange(e.target, this.props.entry)}
                         inputRef={this.projectInput}
                     />
                 </td>
                 <td>
                     <textarea
-                        name="nodes"
+                        name="note"
                         onChange={(e) => this.props.onChange(e.currentTarget, this.props.entry)}
-                        value={this.props.entry.notes}
+                        value={this.props.entry.note}
                         className="form-control form-control-sm"
                     ></textarea>
                 </td>
